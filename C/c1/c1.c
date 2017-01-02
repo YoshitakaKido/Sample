a177b0029c237f18945229fd93250264e28eddd4
root <root@localhost.localdomain>
2017-01-02 21:39:10 +0900

    Test

---
+++ C/c1/c1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+  int a = 234;
+  float b = 2016.1224;
+  char c[] = "Hello, C!";
+
+  printf("%d %f %s \n", a, b, c);
+  
+  return EXIT_SUCCESS;
+}
+
+
---
+++ Ruby/README.md
@@ -0,0 +1,2 @@
+# Ruby
+## Sample code
